f86845bbfbe6d18fb705a0f46f791c560d923cbd
Fernando Catacora <nando.drw@gmail.com>
2014-10-06 15:11:39 -0500

    add test view. addfunctionality to navigate in repos close #3

--- app/_testTimeline/testTimeline.js
+++ app/_testTimeline/testTimeline.js
@@ -11,23 +11,80 @@ angular.module('octolog.timeline', ['ngRoute','octoblogServices'])
     }
   ]).
 
-controller('timelineCtr',['sessionService','githubService','$scope','$interval',function(sessionService,githubService,$scope,$interval){
+controller('testTimelineCtr',['sessionService','githubService','dataService','infoManipulation','$scope','$interval',function(sessionService,githubService,dataService,infoManipulation,$scope,$interval){
 
-	refreshCommitsJob = $interval(function() {
+  $scope.user = sessionService.currentUser;
+  $scope.repos = [];
+  $scope.commitList = [];
+  window.a = $scope.commitList;
 
+	var refreshCommitsJob = $interval(function() {
+    console.log("happening!!");
 	}, 5000);
 
 	refreshCommitsJob.
 	then(function(){
-		console.log("looking for new commits")
-	})
+		console.log("looking for new commits");
+	});
 
 
-  $scope.stopRefrehCommits = function() {
-    if (angular.isDefined(refreshCommitsJob)) {
-      $interval.cancel(refreshCommitsJob);
-      refreshCommitsJob = undefined;
+  // verify if we have data asiciated with the user,
+  // if we don't, call get data from github API
+  dataService.getRepos($scope.user.name).
+  then(function(repos){
+    if(repos.length > 0){
+      $scope.repos = repos;
+      return false
+    } else {
+      return githubService.getUserEvents($scope.user.name,$scope.user.token,1);
     }
+  }).
+  then(function(response){
+    if(response){
+      console.log('response1',response);
+      return githubService.getCommitsFromEvents($scope.user.token,response);
+    } else {
+      return false
+    }
+  }).
+  then(function(response){
+    if(response){
+      console.log('response2',response);
+      dataService.storeCommits($scope.user.name,response).
+      then(function(storedObjects){
+        // some manipulation when the first commits of
+        // a specifict user are stores in the database
+      });
+      return infoManipulation.getReposFromCommits(response);
+    } else {
+      return false
+    }
+  }).
+  then(function(response){
+    if(response){
+      console.log('response3',response);
+      return dataService.storeRepos($scope.user.name,response);
+    } else {
+      return false
+    }
+  }).
+  then(function(response){
+    if(response){
+      console.log('response4',response);
+      console.log('final response',response);
+      $scope.repos = response;
+    }
+  });
+
+  $scope.getCommits = function (repo){
+    console.log('.-.',repo);
+    dataService.getCommits($scope.user.name,repo.name).
+    then(function(commits){
+      console.log('commits from db',commits);
+      $scope.commitList = commits;
+    },function(err){
+      console.log('error!',err);
+    });
   };
 
 }]);
